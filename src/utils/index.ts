b1032c25061442f0bdfce815f847d8eb418daa13
aGFydWtp <haruki@censor.space>
2023-05-07 15:48:24 +0000

    refactor: change method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,8 @@
-import {format} from 'date-fns';
-import {utcToZonedTime} from 'date-fns-tz';
+import {formatInTimeZone} from 'date-fns-tz';
 
 export const formatJstDateString = (
   utcDate: Date,
   dateFormat: string = 'yyyy年MM月dd日 HH:mm'
 ): string => {
-  const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo');
-  return format(jstDate, dateFormat);
+  return formatInTimeZone(utcDate, 'Asia/Tokyo', dateFormat);
 };
